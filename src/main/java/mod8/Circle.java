9c06afe207963e86e599992ac39d8cfb533c5415
alex0 <alex05ap@gmail.com>
2023-10-01 10:13:47 +0300

    Create homework

--- src/main/java/mod8/Circle.java
+++ src/main/java/mod8/Circle.java
@@ -1,19 +1,9 @@
 package mod8;
 
-public class Circle extends Shape {
+public class Circle implements Shape {
 
     @Override
-    public void drawFigure() {
-        System.out.println("Draw shape is circle");
-    }
-
-    @Override
-    public void shapeLine() {
-        System.out.println("Draw shape is line 0,5 pixel");
-    }
-
-    @Override
-    public void shapeColor() {
-        System.out.println("Draw shape color is red");
+    public void printShape() {
+        System.out.println("Circle");
     }
 }
--- src/main/java/mod8/Diamond.java
+++ src/main/java/mod8/Diamond.java
@@ -1,18 +1,9 @@
 package mod8;
 
-public class Diamond extends Shape {
-    @Override
-    public void drawFigure() {
-        System.out.println("Draw shape is diamond");
-    }
+public class Diamond implements Shape {
 
     @Override
-    public void shapeLine() {
-        System.out.println("Draw shape is line 0,1 pixel");
-    }
-
-    @Override
-    public void shapeColor() {
-        System.out.println("Draw shape color is brown");
+    public void printShape() {
+        System.out.println("Diamond");
     }
 }
