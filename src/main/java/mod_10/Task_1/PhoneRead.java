fa974bd6f2f826c47272e15182deb173b5721ab0
alex0 <alex05ap@gmail.com>
2023-10-21 21:22:33 +0300

    Create homework

--- src/main/java/mod_10/Task_1/PhoneRead.java
+++ src/main/java/mod_10/Task_1/PhoneRead.java
@@ -2,28 +2,27 @@ package mod_10.Task_1;
 
 import java.io.*;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class PhoneRead extends Reader {
+public class PhoneRead {
+	private static final String FILE_INPUT_PATH = "src/main/java/mod_10/Task_1/Files/file.txt";
+
 	public static void main(String[] args) throws FileNotFoundException {
-
-		File file = new File("src/main/java/mod_10/Task_1/Files/file.txt");
+		File file = new File(FILE_INPUT_PATH);
 		FileInputStream fis = new FileInputStream(file);
 		Scanner scanner = new Scanner(fis);
-	//	Pattern pattern = new PatternSyntaxException("/n");
+		String regex = "^\\(?(\\d{3})\\)?[-]?\\s?(\\d{3})[-]?(\\d{4})$";
+		Pattern pattern = Pattern.compile(regex);
 		while (scanner.hasNext()){
 			String fileLine = scanner.nextLine();
-
-			System.out.println("fileLine = " + fileLine);
+			Matcher matcher = pattern.matcher(fileLine);
+			while (matcher.find()) {
+				System.out.println(fileLine);
+			}
 		}
 	}
-
-	@Override
-	public int read(char[] cbuf, int off, int len) throws IOException {
-		return 0;
-	}
-
-	@Override
-	public void close() throws IOException {
-
-	}
 }
+
+
+
