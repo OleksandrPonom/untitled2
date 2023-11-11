501d92701c2a99c962ca397b40ea6d1358c9c2e6
alex0 <alex05ap@gmail.com>
2023-11-11 16:19:39 +0200

    Create homework

--- src/main/java/mod_10/Task_2/RefactoringFile.java
+++ src/main/java/mod_10/Task_2/RefactoringFile.java
@@ -1,23 +1,47 @@
 package mod_10.Task_2;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class RefactoringFile {
 	private static final String FILE_INPUT_PATH = "src/main/java/mod_10/Task_2/Files/file.txt";
+	private static final String FILE_OUTPUT_PATH = "src/main/java/mod_10/Task_2/Files/user.json";
 
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		File file = new File(FILE_INPUT_PATH);
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		FileInputStream fis = new FileInputStream(file);
+		Scanner scanner = new Scanner(fis);
+		JsonArray jsonArray = new JsonArray();
 
-		try (FileReader fr = new FileReader(file)) {
-			char[] chars = new char[(int) file.length()];
-			fr.read(chars);
-			String fileContent = new String(chars);
-			System.out.println("fileContent = " + fileContent);
-		} catch (IOException e) {
-			e.printStackTrace();
+		String userName = "";
+		int userAge = 0;
+		String json = "";
+
+		while (scanner.hasNextLine()) {
+			String fileLine = scanner.nextLine();
+			String regex = "[0-9]+";
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(fileLine);
+			while (matcher.find()) {
+				StringBuilder sb = new StringBuilder();
+				userName = String.valueOf(sb.append(fileLine.substring(0, fileLine.indexOf(" "))));
+				userAge = Integer.parseInt(matcher.group());
+				User user = new User(userName, userAge);
+				json = gson.toJson(user);
+				jsonArray.add(json);
+			}
+
+			FileWriter fileWriter = new FileWriter(FILE_OUTPUT_PATH);
+			fileWriter.write(jsonArray.toString());
+			fileWriter.close();
 		}
 	}
 }
--- src/main/java/mod_10/Task_2/User.java
+++ src/main/java/mod_10/Task_2/User.java
@@ -1,6 +1,6 @@
 package mod_10.Task_2;
 
-public class FileStrukture {
+public class User {
 	private String name;
 	private int age;
 
@@ -18,5 +18,18 @@ public class FileStrukture {
 
 	public void setAge(int age) {
 		this.age = age;
+	}
+
+	public User(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"name='" + name + '\'' +
+				", age=" + age +
+				'}';
 	}
 }
